bc4d12f6fc7c18579bb76b950e0df651ba01c46f
Þórdís Þórðardóttir <thordist15@ru.is>
2016-10-13 18:10:34 +0000

    Test two numbers

--- src/main/java/is/ru/stringcalculator/Calculator.java
+++ src/main/java/is/ru/stringcalculator/Calculator.java
@@ -5,6 +5,10 @@ public class Calculator {
 	public static int add(String text){
 		if(text.equals(""))
 				return 0;
+		else if(text.contains(",")){
+			String[] numbers = text.split(",");
+			return Integer.parseInt(numbers[0]) + Integer.parseInt(numbers[1]);
+		}
 		else
 				return 1;
 	}
